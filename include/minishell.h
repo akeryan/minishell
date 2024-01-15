4a41753d015048b0607d274d453ee8c36703c628
Dastan Abdygali <dabdygal@lab1r4s13.42abudhabi.ae>
2024-01-15 19:37:53 +0400

    get_next_token() ready

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dabdygal <dabdygal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 17:28:57 by akeryan           #+#    #+#             */
-/*   Updated: 2024/01/12 16:24:11 by dabdygal         ###   ########.fr       */
+/*   Updated: 2024/01/15 17:05:39 by dabdygal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,14 @@
 
 /** EXIT_MSG - exit message */
 # define EXIT_MSG "\nexit\n"
+
+/** A command line with a cursor, made for succesive tokenizing function */
+typedef struct s_line
+{
+	char	*input;
+	char	*cursor;
+}	t_line;
+
+t_line	ft_readline(const char *prompt);
+
 #endif
--- main.c
+++ main.c
@@ -6,33 +6,41 @@
 /*   By: dabdygal <dabdygal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 19:21:52 by akeryan           #+#    #+#             */
-/*   Updated: 2024/01/15 12:01:27 by dabdygal         ###   ########.fr       */
+/*   Updated: 2024/01/15 19:36:05 by dabdygal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "minishell.h"
 #include <stdio.h>
-#include "readline/readline.h"
 #include "readline/history.h"
 #include <stdlib.h>
+#include "tokens.h"
 
 int	main(void)
 {
-	char	*input;
+	t_line	line;
+	t_token	token;
 
 	while (1)
 	{
-		input = readline(MSH_PROMPT);
-		if (input == NULL)
+		line = ft_readline(MSH_PROMPT);
+		if (line.input == NULL)
 		{
 			if (printf(EXIT_MSG) < 0)
 				return (EXIT_FAILURE);
 			return (EXIT_SUCCESS);
 		}
-		add_history(input);
-		printf("The command entered: %s\n", input);
-		ft_free(input);
+		add_history(line.input);
+		printf("The command entered: %s\n", line.input);
+		token.type = ERROR_TOKEN;
+		while (token.type != END_TOKEN)
+		{
+			token = get_next_token(&line);
+			printf("Token: %i; length: %zu\n", token.type, token.slice.length);
+		}
+		ft_free(line.input);
+		line.cursor = NULL;
 	}
 	return (EXIT_SUCCESS);
 }
