70db027e49662b9b909de6f70f8de27def57bfe9
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-01-24 20:11:39 +0400

    final v1.0

--- include/node.h
+++ include/node.h
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 16:14:32 by akeryan           #+#    #+#             */
-/*   Updated: 2024/01/24 19:02:50 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/01/24 20:10:38 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,17 @@ typedef enum e_node_type
 	IO_HERE_NODE = 9,
 	HERE_END_NODE = 10,
 	LINEBREAK_NODE = 11,
-	NEWLINE_LIST_NODE = 12	
+	NEWLINE_LIST_NODE = 12
 } t_node_type;
 
-typedef char*	t_word_value;
-typedef t_node*	t_cmd_name_value;
-typedef t_node*	t_cmd_word_value;
-typedef t_node*	t_filename_value;
-typedef char*	t_dless_value;
-typedef t_node*	t_here_end_value;
+typedef char	*t_word_value;
+typedef t_node	*t_cmd_name_value;
+typedef t_node	*t_cmd_word_value;
+typedef t_node	*t_filename_value;
+typedef t_node	*t_here_end_value;
+typedef t_node	*t_io_here_value;
+typedef t_node	*t_linebreak_value;
+typedef t_node	*t_newline_list_value;
 
 typedef struct s_node
 {
@@ -59,6 +61,7 @@ typedef struct s_cmd_value
 	t_node	*sufix;
 	t_node	*cmd_name;
 	t_node	*cmd_word;
+	t_node	*linebreak;
 } t_cmd_value;
 
 typedef struct s_prefix_value
@@ -82,29 +85,26 @@ typedef struct s_redir_value
 
 typedef struct s_io_file_value
 {
-	t_node	*io_op;
-	t_node	*filename;
+	e_token_type	op_type;
+	t_node			*filename;
 } t_io_file_value;
-
-typedef struct s_io_here_value
-{
-	t_node	*here_end;
-	t_node	*dless;
-}	t_io_here_value;
 
 typedef union u_node_value
 {
-	t_pipeline_value	pipeline;
-	t_cmd_value			cmd;
-	t_cmd_name_value	cmd_name;
-	t_word_value		word;	
-	t_prefix_value		prefix;
-	t_sufix_value		sufix;
-	t_redir_value		io_redir;
-	t_io_file_value		io_file;
-	t_filename_value	filename;
-	t_dless_value		dless;
-	t_here_end_value	here_end;
+	t_pipeline_value		pipeline;
+	t_cmd_value				cmd;
+	t_cmd_word_value		cmd_word;
+	t_cmd_name_value		cmd_name;
+	t_word_value			word;	
+	t_prefix_value			prefix;
+	t_sufix_value			sufix;
+	t_redir_value			io_redir;
+	t_io_file_value			io_file;
+	t_filename_value		filename;
+	t_here_end_value		here_end;
+	t_io_here_value			io_here;
+	t_linebreak_value		linebreak;
+	t_newline_list_value	newline_list;
 } t_node_value;
 
 #endif
