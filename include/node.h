7f302c83cb0bccbdbe9407df4e1364255853257c
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-01-25 14:37:37 +0400

    norminated

--- include/node.h
+++ include/node.h
@@ -6,33 +6,33 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 16:14:32 by akeryan           #+#    #+#             */
-/*   Updated: 2024/01/25 14:32:08 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/01/25 14:37:08 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NODE_H
-#define NODE_H
+# define NODE_H
 
-#include "tokens.h"
+# include "tokens.h"
 
 typedef enum e_node_type
 {
-	PROGRAM = 0, 
+	PROGRAM = 0,
 	PIPELINE = 1,
 	COMMAND = 2,
 	PREFIX = 4,
 	SUFIX = 5,
 	IO_REDIR = 6,
 	NEWLINE_LIST = 12
-} t_node_type;
+}	t_node_type;
 
 typedef struct s_node
 {
-	t_node_type		node_type;
 	char			*word;
+	t_node_type		node_type;
 	t_token_type	token_type;
-	struct s_node	*left;
-	struct s_node	*right;
-} t_node;
+	t_node			*left;
+	t_node			*right;
+}	t_node;
 
 #endif
