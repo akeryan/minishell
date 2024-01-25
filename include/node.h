cd8aa8127f2697b2385c03d59c90148cbc0b689a
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-01-25 15:36:20 +0400

    added comments

--- include/node.h
+++ include/node.h
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 16:14:32 by akeryan           #+#    #+#             */
-/*   Updated: 2024/01/25 14:38:24 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/01/25 15:35:33 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,34 @@
 
 # include "tokens.h"
 
+/** Types of nodes */
 typedef enum e_node_type
 {
 	PROGRAM = 0,
 	PIPELINE = 1,
 	COMMAND = 2,
 	PREFIX = 3,
-	SUFIX = 4,
+	SUFFIX = 4,
 	IO_REDIR = 5,
 	NEWLINE_LIST = 6
 }	t_node_type;
 
+/**
+ * Covers architectures of all types of nodes from e_node_type
+ * - See the 'Grammar' document to distinguish between node structures
+ * - e.g. 'COMMAND' node uitlizes the following attributes
+ * -- 'left' for 'prefix' node
+ * -- 'word' for 'command name'
+ * -- 'right' for 'suffix' node
+ * -- the rest of the attributes are left uninitialized;
+*/
 typedef struct s_node
 {
 	char			*word;
 	t_node_type		node_type;
 	t_token_type	token_type;
-	t_node			*left;
-	t_node			*right;
+	struct s_node	*left;
+	struct s_node	*right;
 }	t_node;
 
 #endif
