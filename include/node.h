9fbd1b2b7c6ccc1ffc69b9b6e6866ff3bd835c0f
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-01-25 14:38:49 +0400

    updated t_node_type

--- include/node.h
+++ include/node.h
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 16:14:32 by akeryan           #+#    #+#             */
-/*   Updated: 2024/01/25 14:37:08 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/01/25 14:38:24 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ typedef enum e_node_type
 	PROGRAM = 0,
 	PIPELINE = 1,
 	COMMAND = 2,
-	PREFIX = 4,
-	SUFIX = 5,
-	IO_REDIR = 6,
-	NEWLINE_LIST = 12
+	PREFIX = 3,
+	SUFIX = 4,
+	IO_REDIR = 5,
+	NEWLINE_LIST = 6
 }	t_node_type;
 
 typedef struct s_node
