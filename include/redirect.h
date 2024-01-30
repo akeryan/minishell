854d1f2e4b0861a40db9836005ea06397f8ca3a2
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-01-30 15:26:35 +0400

    redirect.h added

---
+++ include/redirect.h
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 14:05:46 by akeryan           #+#    #+#             */
+/*   Updated: 2024/01/30 14:06:30 by akeryan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void redir_read(char *file_name);
+void redir_write(char *file_name);
+void redir_append(char *file_name);
