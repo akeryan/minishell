fab3feb10f1cbd5a05e0dc9e0cb25a728c9212ea
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-02-06 19:17:47 +0400

    tilde_expansion added: unfinished

--- src/execute/expansion.c
+++ src/execute/expansion.c
@@ -6,11 +6,30 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 16:18:15 by akeryan           #+#    #+#             */
-/*   Updated: 2024/02/06 16:26:37 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/02/06 19:13:13 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 char	*expand(char *word)
 {
 
+
+}
+
+tilde_expansion(char **word)
+{
+	char	*tail;
+
+	tail = NULL;
+	if (*word[0] == '~')
+	{
+		slash = ft_strchr('/');
+		if (slash)
+			tail = slash;
+		else
+			tail = *word[1];
+	}
+	out = ft_strjoin(getenv("HOME"), tail);
 }
