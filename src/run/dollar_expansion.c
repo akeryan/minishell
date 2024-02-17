9585be07d50108eeacaae09b0c0a88e884735ded
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-02-17 13:21:06 +0400

    tilde update

--- src/run/dollar_expansion.c
+++ src/run/dollar_expansion.c
@@ -1,25 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expansion.h                                        :+:      :+:    :+:   */
+/*   dollar_expansion.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/16 13:19:10 by akeryan           #+#    #+#             */
-/*   Updated: 2024/02/17 13:15:14 by akeryan          ###   ########.fr       */
+/*   Created: 2024/02/17 13:14:10 by akeryan           #+#    #+#             */
+/*   Updated: 2024/02/17 13:14:13 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXPANSION_H
-# define EXPANSION_H
-
-# include <stdbool.h>
-
-int		apply_expansions(char **word, char ***env);
-void	tilde_expansion(char **word, char ***env);
-void	dollar_expansion(char **word, char ***env);
-bool	is_quoted(const char *str, const char *target);
-char	*is_there_unquoted_slash(char *word);
-char	*ft_getenv(char *word, char **env);
-
-#endif
