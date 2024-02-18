904cb3fd19dd28354349d2d195f2bfb70131188e
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-02-18 23:24:46 +0400

    dollar tokenization organized

--- src/run/dollar_expansion.c
+++ src/run/dollar_expansion.c
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 14:55:06 by akeryan           #+#    #+#             */
-/*   Updated: 2024/02/18 21:48:22 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/02/18 23:23:09 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,68 +15,89 @@
 #include "libft.h"
 #include "word_list.h"
 
+static int	func_1(char **current, t_word_node **head)
+{
+	if (ft_strlen(*current) > 0)
+		add_word_back(head, new_word(*current));
+	return (1);
+}
+
+static void	func_2(t_word_node **head, char **current, char **ptr)
+{
+	add_word_back(head, new_word_m(ft_strdup(*current, *ptr - *current)));
+	*current = *ptr;
+}
+
+static void	func_3(char **ptr, t_word_node **head, t_data *data, char **current)
+{
+	if (*(*ptr + 1) == '$')
+		add_word_back(head, new_word_m(ft_itoa(data->dbl_dollar)));
+	else
+		add_word_back(head, new_word_m(ft_itoa(data->exit_status)));
+	*ptr += 2;
+	*current = *ptr;
+}
+
+static void	func_4(char **ptr, char **current, t_word_node **head)
+{
+	while (*(*ptr + 1) != '$' && *(*ptr + 1) != '\0' && *(*ptr + 1) \
+		!= '"' && *(*ptr + 1) != '\'')
+		(*ptr)++;
+	(*ptr)++;
+	if (*ptr != *current)
+	{
+		add_word_back(head, new_word_m(ft_strdup(*current, *ptr - *current)));
+		*current = *ptr;
+	}
+}
+
+static int	func_5(char **ptr, char **current)
+{
+	if ((*(*ptr + 1) == '\0' || *(*ptr + 1) == '"') && \
+		!ft_strchr(*current + 1, '$'))
+		return (1);
+	else
+		return (0);
+}
+
+static int func_6(t_word_node **head, char **current)
+{
+	add_word_back(head, new_word(*current));
+	return (1);
+}
+
+static int func_7(t_word_node **head)
+{
+	add_word_back(head, new_word("$"));
+	return (1);
+}
+
 void	dollar_expansion(char **word, t_data *data)
 {
 	char		*ptr;
 	char		*current;
 	t_word_node	*head;	
-	t_word_node *tmp;
 
-	if (!word)
-		return ;
 	current = *word;
 	while (1)
 	{
 		ptr = is_there_unquoted_dollar(current);
-		if (!ptr)
-		{
-			if (ft_strlen(current) > 0)
-				add_word_back(&head, new_word(current));	
+		if (!ptr && func_1(&current, &head))
 			break ;
-		}
 		if (ptr != current)
 		{
-			if ((*(ptr + 1) == '\0' || *(ptr + 1) == '"') && !ft_strchr(current + 1, '$'))
-			{
-				add_word_back(&head, new_word(current));	
+			if (func_5(&ptr, &current) && func_6(&head, &current))
 				break ;
-			}
-			add_word_back(&head, new_word_m(ft_strdup(current, ptr - current)));
-			current = ptr;
+			func_2(&head, &current, &ptr);
 		}
-		if (*(ptr + 1) == '\0')
-		{
-			add_word_back(&head, new_word("$"));
+		if (*(ptr + 1) == '\0' && func_7(&head))
 			break ;
-		}
 		if (*(ptr + 1) == '$' || *(ptr + 1) == '?')
 		{
-			if (*(ptr + 1) == '$')
-				add_word_back(&head, new_word_m(ft_itoa(data->dbl_dollar)));
-			else
-				add_word_back(&head, new_word_m(ft_itoa(data->exit_status)));
-			ptr+=2;
-			current = ptr;
-			continue;
+			func_3(&ptr, &head, data, &current);
+			continue ;
 		}
-		while(*(ptr + 1) != '$' && *(ptr +1) != '\0' && *(ptr + 1) != '"' && *(ptr + 1) != '\'')
-			ptr++;
-		ptr++;
-		if (ptr != current)
-		{
-			add_word_back(&head, new_word_m(ft_strdup(current, ptr - current)));
-			current = ptr;
-		}
-	}
-	tmp = head;
-	while (1)
-	{
-		if (tmp)
-			printf("EXIT: %s\n", tmp->word);
-		if (tmp->next)
-			tmp = tmp->next;
-		else
-			break;
+		func_4(&ptr, &current, &head);
 	}
 }
 
