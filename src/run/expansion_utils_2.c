b44f0e3cf222fec215a85cdc6715cb95de9962e2
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-02-20 17:39:02 +0400

    remove_quotation_marks() words

--- src/run/expansion_utils_2.c
+++ src/run/expansion_utils_2.c
@@ -6,12 +6,13 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:06:26 by akeryan           #+#    #+#             */
-/*   Updated: 2024/02/20 16:27:54 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/02/20 17:36:48 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/stat.h>
 #include <stdio.h>
+#include "libft.h"
 #include "expansion.h"
 #include "data.h"
 
@@ -24,8 +25,25 @@ int	does_dir_exist(const char *path)
 	return (S_ISDIR(status.st_mode));
 }
 
+void	remove_quotation_marks(char **word)
+{
+	char	*str;
+
+	str = *word;
+	while (*str != '\0')	
+	{
+		if (*str == '"' || *str == '\'')
+		{
+			ft_strlcpy(str, str + 1, ft_strlen(str));			
+			continue ;
+		}
+		str++;		
+	}
+}
+
 void	apply_expansions(char **word, t_data *d)
 {
 	tilde_expansion(word, d->env);
 	dollar_expansion(word, d);
+	remove_quotation_marks(word);
 }
